6a850b1486258a313b0d89bff878e3c30c8a5f11
Tanaka Hideyuki <thdyk.4.11@gmail.com>
2023-06-01 23:18:54 +0900

    Create util.rs and add chmin, chmax

---
+++ src/util.rs
@@ -0,0 +1,3 @@
+// macros
+macro_rules! chmin { ($a: expr, $b: expr) => { if $a > $b { $a = $b; true } else { false } }; }
+macro_rules! chmax { ($a: expr, $b: expr) => { if $a < $b { $a = $b; true } else { false } }; }
